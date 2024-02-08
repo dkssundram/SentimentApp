6b3b44eb9eb59b7063f15f379f60330d63867b6d
Deepak Kumar Sundram <110852589+dkssundram@users.noreply.github.com>
2024-02-08 18:44:36 +0530

    commit to deploy

--- app.py
+++ app.py
@@ -30,5 +30,5 @@ def sentiment_analysis():
 
     return render_template('index.html', result=result, user_input=user_input)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+#    app.run(debug=True)
